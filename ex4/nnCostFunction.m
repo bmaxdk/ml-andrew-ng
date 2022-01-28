377dcc5b09bc9f2d66b3f0a37ee9ec1ce537fe2a
bmaxdk <choh8uw@gmail.com>
2022-01-27 19:35:40 -0800

    Update ex4

--- ex4/nnCostFunction.m
+++ ex4/nnCostFunction.m
@@ -14,6 +14,9 @@ function [J grad] = nnCostFunction(nn_params, ...
 %   partial derivatives of the neural network.
 %
 
+
+
+
 % Reshape nn_params back into the parameters Theta1 and Theta2, the weight matrices
 % for our 2 layer neural network
 Theta1 = reshape(nn_params(1:hidden_layer_size * (input_layer_size + 1)), ...
@@ -27,8 +30,8 @@ m = size(X, 1);
          
 % You need to return the following variables correctly 
 J = 0;
-Theta1_grad = zeros(size(Theta1));
-Theta2_grad = zeros(size(Theta2));
+Theta1_grad = zeros(size(Theta1)); % 25 x 401
+Theta2_grad = zeros(size(Theta2)); % 10 x 26
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: You should complete the code by working through the
@@ -65,27 +68,66 @@ Theta2_grad = zeros(size(Theta2));
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+% size(X) %5000 X 400
+% size(y) %5000 X 1
+% size(nn_params) %10285 X 1
+% hidden_layer_size %25
+% num_labels %10
 % -------------------------------------------------------------
 
 % =========================================================================
 
+
+
+%% Update y  
+% original labels (in the variable y) were  for the purpose of training a neural network, we need to recode the labels as vectors containing only values 0 or 1,
+ny = zeros(size(y,1),num_labels);
+for i=1:m
+  ny(i,y(i)) = 1;
+end
+
+
+
+%% forward
+a1 = [ones(m,1) X];
+z2 = a1 * Theta1';
+a2 = sigmoid(z2);
+a2 = [ones(m,1) a2];
+z3 = a2 * Theta2';
+a3 = sigmoid(z3); % 5000 x 10
+
+%% Regularization
+regularization = (lambda/(2*m))*(sum(sum(Theta1(:,2:end).^2)) + sum(sum(Theta2(:,2:end).^2)));
+
+
+
+%% cost
+% sum(-ny .* log(h3)) %sum each row also can use sum(-ny .* log(h3),1)
+J = (1/m) * sum(sum((-ny .* log(a3)) - ((1-ny).*log(1-a3)))) + regularization;
+
+
+%% Backpropagation
+a3; %5000x10
+ny; %5000x10
+y; %5000x1
+delta_3 = a3 - ny; %5000 x 10
+delta_2 = (Theta2(:,2:end))' * delta_3' .* sigmoidGradient(z2'); % 25x5000
+
+%% change
+D_1 = delta_2 * a1; %[25x5000] * [5000x401] = [25x401]
+D_2 = delta_3' * a2;  %[5000x10] * [5000x26] = [10x26]
+% Theta1_grad = (1/m)*(D_1);
+% Theta2_grad = (1/m)*(D_2);
+%% add reg in change
+reg_1 = lambda*[zeros(size(Theta1,1),1) Theta1(:,2:end)]; %[25x401]
+reg_2 = lambda*[zeros(size(Theta2,1),1) Theta2(:,2:end)]; %[10x26]
+
+
+%% gradients
+Theta1_grad = (1/m)*(D_1+reg_1);
+Theta2_grad = (1/m)*(D_2+reg_2);
+
 % Unroll gradients
 grad = [Theta1_grad(:) ; Theta2_grad(:)];
-
 
 end
--- ex4/randInitializeWeights.m
+++ ex4/randInitializeWeights.m
@@ -19,11 +19,8 @@ W = zeros(L_out, 1 + L_in);
 % Note: The first column of W corresponds to the parameters for the bias unit
 %
 
-
-
-
-
-
+epsilion_init = 0.12;
+W = rand(L_out, 1 + L_in) * 2 * epsilion_init - epsilion_init;
 
 
 
